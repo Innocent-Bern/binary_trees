615afc01ec7dfd9ff9e116818b3d15b8fe311652
Innocent_Bern <innocentbern22@gmail.com>
2023-08-31 20:30:18 +0300

    task-16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,5 +16,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_depth(tree->right) == binary_tree_depth(tree->left))
 		return (1);
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) 
+	&& binary_tree_is_perfect(tree->right));
 }
