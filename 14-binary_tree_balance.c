f67381ec204eb55f6791565363e0225f8b3dfd10
Innocent_Bern <innocentbern22@gmail.com>
2023-08-31 12:44:19 +0300

    changes task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - measure balance factor of a binary_tree
@@ -13,9 +14,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		h_left = binary_tree_balance(tree->left) + 1;
+		h_left = binary_tree_height(tree->left) + 1;
 	if (tree->right != NULL)
-		h_right = binary_tree_balance(tree->right) - 1;
+		h_right = binary_tree_height(tree->right) + 1;
 
-	return (h_left + h_right);
+	return (h_left - h_right);
 }
